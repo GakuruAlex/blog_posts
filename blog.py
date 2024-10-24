1a7db273cb941dfc04076b45e3d63adf61cd1f85
alexgicimo <gakurualexgicimo@gmail.com>
2024-10-24 18:38:35 +0300

    process all blogs

--- blog.py
+++ blog.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
+from get_blogs import Blogs
 
 app = Flask(__name__)
 
 
 @app.route("/blogs")
 def blogs():
-    return render_template("index.html")
+    my_blogs = Blogs()
+    my_blogs.load_blogs()
+    return render_template(template_name_or_list="all_blogs.html", blogs=my_blogs.blogs)
 
 
 if __name__ == "__main__":
